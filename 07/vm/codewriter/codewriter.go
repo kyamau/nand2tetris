7cf6238cc08e6cc197547a0a161389a860da0657
kyamau <kyamau.cs@gmail.com>
2022-01-15 23:50:40 +0900

    Refactor pop and push by moving them to functions

--- 07/vm/codewriter/codewriter.go
+++ 07/vm/codewriter/codewriter.go
@@ -8,25 +8,34 @@ import (
 	"vm/parser"
 )
 
+func pushD(code *bytes.Buffer) {
+	code.WriteString("@SP\r\n")
+	code.WriteString("A=M\r\n")
+	code.WriteString("M=D\r\n")
+	code.WriteString("@SP\r\n")
+	code.WriteString("M=M+1\r\n")
+}
+
+func popToD(code *bytes.Buffer) {
+	code.WriteString("@SP\r\n")
+	code.WriteString("M=M-1\r\n")
+	code.WriteString("A=M\r\n")
+	code.WriteString("D=M\r\n")
+}
+
 func WriteArithmetic(op parser.ALOperator) string {
 	var code bytes.Buffer
 	// POP from the stack to R13
-	code.WriteString("@SP\r\n")
-	code.WriteString("M=M-1\r\n")
-	code.WriteString("A=M\r\n")
-	code.WriteString("D=M\r\n")
-	code.WriteString("@13\r\n") // Load poped item to R13
+	popToD(&code)
+	code.WriteString("@13\r\n")
 	code.WriteString("M=D\r\n")
 	// POP from the stack to R14
-	code.WriteString("@SP\r\n")
-	code.WriteString("M=M-1\r\n")
-	code.WriteString("A=M\r\n")
-	code.WriteString("D=M\r\n")
-	code.WriteString("@14\r\n") // Load poped item to R14
+	popToD(&code)
+	code.WriteString("@14\r\n")
 	code.WriteString("M=D\r\n")
-	switch op {
 
 	// Caluculate and load the result to D
+	switch op {
 	case parser.ADD:
 		code.WriteString("@14\r\n")
 		code.WriteString("D=M\r\n")
@@ -39,11 +48,7 @@ func WriteArithmetic(op parser.ALOperator) string {
 		code.WriteString("D=D-M\r\n")
 	}
 	// Push D to the stack
-	code.WriteString("@SP\r\n")
-	code.WriteString("A=M\r\n")
-	code.WriteString("M=D\r\n")
-	code.WriteString("@SP\r\n")
-	code.WriteString("M=M+1\r\n")
+	pushD(&code)
 
 	return code.String()
 }
@@ -69,31 +74,32 @@ func segment2Symbol(segment string) string {
 	return ""
 }
 
+func setAddressToD(code *bytes.Buffer, segment string, index int) {
+	segsym := segment2Symbol(segment)
+	code.WriteString(fmt.Sprintf("@%v\r\n", index))
+	code.WriteString("D=A\r\n")
+	code.WriteString(fmt.Sprintf("@%v\r\n", segsym))
+	switch segment {
+	case "local", "argument", "this", "that":
+		code.WriteString("D=D+M\r\n")
+	case "pointer", "temp":
+		code.WriteString("D=D+A\r\n")
+	}
+}
+
 func WritePushPop(cmdType parser.CommandType, segment string, index int) string {
 	var code bytes.Buffer
 	switch cmdType {
 	case parser.C_POP:
 
-		// Load from the stack and SP--
-		code.WriteString("@SP\r\n")
-		code.WriteString("M=M-1\r\n")
-		code.WriteString("A=M\r\n")
-		code.WriteString("D=M\r\n")
+		// Pop to R13
+		popToD(&code)
 		code.WriteString("@13\r\n") // Load poped item to R13
 		code.WriteString("M=D\r\n")
 
-		// Calculate segment + i
-		segsym := segment2Symbol(segment)
-		code.WriteString(fmt.Sprintf("@%v\r\n", index))
-		code.WriteString("D=A\r\n")
-		code.WriteString(fmt.Sprintf("@%v\r\n", segsym))
-		switch segment {
-		case "local", "argument", "this", "that":
-			code.WriteString("D=D+M\r\n")
-		case "pointer", "temp":
-			code.WriteString("D=D+A\r\n")
-		}
-		code.WriteString("@14\r\n") // Load destination address to R14
+		// Calculate segment + i and set it to R14
+		setAddressToD(&code, segment, index)
+		code.WriteString("@14\r\n") // Load poped item to R13
 		code.WriteString("M=D\r\n")
 
 		// Write the poped value to the destination
@@ -110,26 +116,13 @@ func WritePushPop(cmdType parser.CommandType, segment string, index int) string 
 			code.WriteString(fmt.Sprintf("@%v\r\n", index))
 			code.WriteString("D=A\r\n")
 		default:
-			segsym := segment2Symbol(segment)
-			code.WriteString(fmt.Sprintf("@%v\r\n", index))
-			code.WriteString("D=A\r\n")
-			code.WriteString(fmt.Sprintf("@%v\r\n", segsym))
-			switch segment {
-			case "local", "argument", "this", "that":
-				code.WriteString("D=D+M\r\n")
-			case "pointer", "temp":
-				code.WriteString("D=D+A\r\n")
-			}
+			setAddressToD(&code, segment, index)
 			code.WriteString("A=D\r\n")
 			code.WriteString("D=M\r\n")
 		}
 
 		// Push
-		code.WriteString("@SP\r\n")
-		code.WriteString("A=M\r\n")
-		code.WriteString("M=D\r\n")
-		code.WriteString("@SP\r\n")
-		code.WriteString("M=M+1\r\n")
+		pushD(&code)
 	}
 	return code.String()
 
